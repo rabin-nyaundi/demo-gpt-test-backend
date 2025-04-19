13438cc13e5cc3a68aab2153947f7c5dc26636e4
rabin-nyaundi <nyaundirabin@gmail.com>
2025-04-19 11:33:19 +0300

    add vercel frontend tp allowed origins

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 """ Middleware for CORS"""
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://pawait-gpt.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
